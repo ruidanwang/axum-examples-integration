d63ef56fa5fad83671a78d6c56f677f81c764c15
ruidanwang <newbiner@gmail.com>
2024-07-12 14:33:03 +0800

    bug fix

--- src/rudis/mod.rs
+++ src/rudis/mod.rs
@@ -11,7 +11,7 @@ pub fn app(rudis_pool: Pool<RedisConnectionManager>) -> axum::Router {
     let rudis_router = axum::Router::new()
         .route("/get/:id", get(rudis::get_value))
         .route("/set", get(rudis::set_key_value))
-        .route("/lowercmd/:key", get(rudis::lower_cmd))
+        // .route("/lowercmd/:key", get(rudis::lower_cmd))
         .with_state(rudis_pool)
     ;
     axum::Router::new().nest("/rudis", rudis_router)
--- src/rudis/rudis.rs
+++ src/rudis/rudis.rs
@@ -1,26 +1,16 @@
 use std::collections::HashMap;
 
-use anyhow::Ok;
 use axum::{
-    async_trait,
-    extract::{FromRef, FromRequestParts, Path, Query, State},
-    http::{request::Parts, StatusCode},
+    extract::{Path, Query, State},
+    http::StatusCode,
 };
-use bb8::{Pool, PooledConnection};
-use bb8_redis::{RedisConnectionManager,redis::cmd,};
+use bb8::Pool;
+use bb8_redis::RedisConnectionManager;
 use redis::AsyncCommands;
 
 use bb8_redis::bb8;
-use serde::{Deserialize,Serialize};
 
 type ConnectionPool = Pool<RedisConnectionManager>;
-
-#[derive(Deserialize,Serialize)]
-pub struct Rudis{
-    cmd:String,
-    key:String,
-    value:String
-}
 
 pub async fn get_value(
     Path(key): Path<String>,
@@ -35,51 +25,14 @@ pub async fn set_key_value(
     Query(parms): Query<HashMap<String, String>>,
     State(pool): State<ConnectionPool>,
 ) -> Result<String, (StatusCode, String)> {
-    let key = &parms.get("key").unwrap();
-    let value = &parms.get("value").unwrap();
+    let key = parms.get("key").unwrap();
+    let value = parms.get("value").unwrap();
     let mut conn = pool.get().await.map_err(internal_error)?;
     let result: String = conn.set(key,value).await.map_err(internal_error)?;
     Ok(result)
 }
 
-pub async fn lower_cmd(
-    Path(key): Path<String>,
-    State(pool): State<ConnectionPool>,
-) -> Result<String, (StatusCode, String)> {
-    let mut conn = pool.get().await.map_err(internal_error)?;
-    let result = cmd("get").arg(&key).query_async(&mut *conn).await.map_err(internal_error)?;
-    Ok(result)
-}
 
-
-// we can also write a custom extractor that grabs a connection from the pool
-// which setup is appropriate depends on your application
-pub struct DatabaseConnection(PooledConnection<'static, RedisConnectionManager>);
-
-#[async_trait]
-impl<S> FromRequestParts<S> for DatabaseConnection
-where
-    ConnectionPool: FromRef<S>,
-    S: Send + Sync,
-{
-    type Rejection = (StatusCode, String);
-
-    async fn from_request_parts(_parts: &mut Parts, state: &S) -> Result<Self, Self::Rejection> {
-        let pool = ConnectionPool::from_ref(state);
-
-        let conn = pool.get_owned().await.map_err(internal_error)?;
-
-        Ok(Self(conn))
-    }
-}
-
-pub async fn using_connection_extractor(
-    DatabaseConnection(mut conn): DatabaseConnection,
-) -> Result<String, (StatusCode, String)> {
-    let result: String = conn.get("foo").await.map_err(internal_error)?;
-
-    Ok(result)
-}
 
 /// Utility function for mapping any error into a `500 Internal Server Error`
 /// response.
